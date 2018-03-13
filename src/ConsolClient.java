0342c6ea3a5ad40c1355b05e2a1a3476115a06b7
vonScholten <f.vonscholten@gmail.com>
2018-03-13 14:21:22 +0100

    initial commit

---
+++ src/ConsolClient.java
@@ -0,0 +1,52 @@
+
+import java.rmi.Naming;
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Alt
+ */
+public class ConsolClient {
+
+    static Scanner scanner = new Scanner(System.in);
+    static String input;
+    static String user;
+    static String passwd;
+
+    public static void main(String[] arg) throws Exception {
+        ConsolI con = (ConsolI) Naming.lookup("rmi://localhost:1099/ConsolApplication");
+        
+        System.out.println("enter user");
+        user = scanner.nextLine();
+        System.out.println("enter password");
+        passwd = scanner.nextLine();
+        System.out.println(con.login(user, passwd));
+        
+        while (true) {
+            System.out.println("enter command");
+            input = scanner.nextLine();
+
+            if ("add".equals(input)) {
+                System.out.println("enter name");
+                input = scanner.nextLine();
+                con.addData(input);
+                System.out.println(con.getData());
+            } else if ("remove".equals(input)) {
+                System.out.println("enter name");
+                input = scanner.nextLine();
+                con.removeData(input);
+                System.out.println(con.getData());
+            } else {
+                System.err.println("unknown command");
+            }
+
+        }
+
+    }
+
+}
